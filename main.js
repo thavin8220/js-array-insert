244b7f0ff985c052bfe690f84d7787bb7e86a147
thavin8220 <thavinmurugan@gmail.com>
2022-12-20 17:03:56 +0530

    add file

---
+++ main.js
@@ -0,0 +1,17 @@
+let arrlength=parseInt(prompt("enter the value"));
+let arrvalue=[];
+let c;
+    for(i=0;i<arrlength;i++){
+        let a=prompt("enter the food");
+        document.write(arrvalue[i]=a)
+        document.write("<br>")
+    }
+     c=prompt("enter the value");
+     let b=prompt("enter the");
+     arrvalue[c]=b
+     document.write(arrvalue);
+
+
+     
+
+    
